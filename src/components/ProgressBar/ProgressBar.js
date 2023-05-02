495eb7743061e0e6c96e98befcb7c88ee836dba6
Tim Bacon <tim.bacon@vetprod.com>
2023-05-02 15:51:10 +0200

    working on select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,61 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import clamp from '../../utils/clamp';
+
+const SIZESTYLES = {
+    small: {
+        '--height': '8px',
+        '--border-radius': '4px',
+    },
+    medium: {
+        '--height': '12px',
+        '--border-radius': '4px',
+    },
+    large: {
+        '--height': '24px',
+        '--border-radius': '8px',
+        '--padding': '4px',
+    },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+    const styles = SIZESTYLES[size];
+    const clampedValue = clamp(value, 0, 100);
+    return (
+        <Wrapper
+            style={styles}
+            role='progressbar'
+            aria-valuenow={clampedValue}
+            aria-label='progress'
+        >
+            <BarWrapper>
+                <Bar width={clampedValue} />
+            </BarWrapper>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    border-radius: var(--border-radius);
+    background-color: ${COLORS.transparentGray15};
+    padding: var(--padding);
+    height: var(--height);
+`;
+
+const BarWrapper = styled.div`
+    height: 100%;
+    width: 100%;
+    border-radius: 4px;
+    overflow: clip;
+`;
+
+const Bar = styled.div`
+    height: 100%;
+    width: ${props => props.width + '%'};
+    background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
+
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    return (
+        <Wrapper>
+            <SelectElem value={value} onChange={onChange}>
+                {children}
+            </SelectElem>
+            <SelectIcon id='chevron-down' />
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    position: relative;
+    width: fit-content;
+`;
+
+const SelectElem = styled.select`
+    -webkit-appearance: none;
+    padding: 12px 40px 12px 16px;
+    background-color: ${COLORS.transparentGray15};
+    border: none;
+    border-radius: 8px;
+    color: ${COLORS.gray700};
+
+    &:hover {
+        color: ${COLORS.black};
+    }
+`;
+
+const SelectIcon = styled(Icon)`
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    height: 24px;
+    margin-block: auto;
+`;
 
 export default Select;
